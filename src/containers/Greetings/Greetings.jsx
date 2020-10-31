8f63063db7b28a15e52000d7c158c6b08e7ba325
karenyang <kaiyuany03@gmail.com>
2020-10-30 22:13:39 -0700

    could not do overflow again

--- src/containers/Greetings/Greetings.jsx
+++ src/containers/Greetings/Greetings.jsx
@@ -10,38 +10,39 @@ import {
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import icon from '../../assets/img/icon-34.png';
 import './Greetings.css';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx';
+
 
 const useStyles = makeStyles((theme) => ({
-    // root: {
-    //     display: 'flex',
-    //     flexDirection: 'column',
-    // },
+    root: {
+        display: 'flex',
+        marginTop: 20,
+    },
     paper: {
         color: theme.palette.text.secondary,
         width: 400,
         maxHeight: 450,
         overflow: 'auto',
     },
-    card: {
-        display: "flex",
+    expandOpen: {
+        transform: 'rotate(180deg)',
     },
-    prouct_img: {
-        display: 'flex',
-        width: 100,
-    },
-    product_details: {
-        display: 'flex',
-    },
-    content: {
-        width: 250,
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
     },
 }));
-
 
 
 class Greetings extends Component {
@@ -51,13 +52,21 @@ class Greetings extends Component {
             user_name: this.props.user_name,
             user_id: this.props.user_id,
             self_product_list: [],
+            product_expanded: {},
         }
         console.log(this.state);
     }
 
+
     updateSelfProductList = (product_list) => {
+        let product_expanded = []
+        for (let i = 0; i < product_list.length; i++){
+            const product = product_list[i];
+            product_expanded[product._id] = false;
+        }
         this.setState({
             self_product_list: product_list,
+            product_expanded: product_expanded,
         });
         console.log("State product list updated: ", this.state);
     }
@@ -120,6 +129,16 @@ class Greetings extends Component {
         );
     }
 
+    handleExpandClick = (product_id) => {
+        console.log('Expand Clicked');
+        let new_product_expanded = this.state.product_expanded;
+        Object.assign(new_product_expanded, {product_id: !new_product_expanded[product_id]}); //toggle
+        this.setState({
+            product_expanded: new_product_expanded
+        });
+    }
+
+
 
     render() {
         const classes = this.props;
@@ -143,24 +162,45 @@ class Greetings extends Component {
                         <Paper style={classes.paper}>
                             {
                                 this.state.self_product_list.map((product) => (
-                                    <Card key={product._id} className={classes.card}>
-                                        <div >
+                                    <Card key={product._id} m={50}>
+                                        <Grid container spacing={3} >
+                                            <Grid item xs={4}>
+                                                <CardActionArea>
+                                                    <img alt={product.product_title} src={product.product_imgurl} width="100" onClick={() => { this.onClickProduct(product) }} />
+                                                </CardActionArea>
+                                            </Grid>
+                                            <Grid item xs={8}>
+                                                <CardContent >
+                                                    <Typography gutterBottom variant="body2" component="h5">
+                                                        {(product.product_title)}
+                                                    </Typography>
 
-                                        <CardActionArea style={{maxWidth:120}}>
-                                            <img src={product.product_imgurl} alt={product.product_title} width="100" onClick={() => { this.onClickProduct(product) }} />
-                                        </CardActionArea>
-                                        </div>
-                                        <div>
-                                        <CardContent style={{maxWidth:200}} className={classes.content}>
-                                            <Typography gutterBottom variant="body2" component="h5">
-                                                {(product.product_title).substring(0, 15)}
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary" component="p">
-                                                ${product.product_cost}
-                                            </Typography>
-                                        </CardContent>
-                                        </div>
+                                                    <Typography variant="body2" color="textSecondary" component="p">
+                                                        ${product.product_cost}
+                                                    </Typography>
+                                                </CardContent>
 
+                                                <CardActions disableSpacing>
+                                                    <IconButton aria-label="add to favorites">
+                                                        <FavoriteIcon />
+                                                    </IconButton>
+                                                    <IconButton aria-label="share">
+                                                        <ShareIcon />
+                                                    </IconButton>
+                                                    <IconButton
+                                                        className={clsx(classes.expand, {
+                                                            [classes.expandOpen]: this.state.product_expanded[product._id],
+                                                        })}
+                                                        onClick={this.handleExpandClick}
+                                                        aria-expanded={this.state.product_expanded[product._id]}
+                                                        aria-label="show more"
+                                                    >
+                                                        <ExpandMoreIcon />
+                                                    </IconButton>
+                                                </CardActions>
+                                            </Grid>
+
+                                        </Grid>
                                     </Card>
                                 ))
                             }
