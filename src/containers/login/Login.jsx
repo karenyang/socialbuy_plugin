22b9c4c5bb4ed95479cb6b4f886f844e757c573b
Karen <kaiyuanyang@Kaiyuans-MacBook-Pro.local>
2020-11-24 21:21:10 -0800

    lost cause

--- src/containers/login/Login.jsx
+++ src/containers/login/Login.jsx
@@ -11,8 +11,9 @@ import {
 import icon from '../../assets/img/icon-34.png';
 import FacebookLogin from 'react-facebook-login';
 import FacebookLoginWithButton from 'react-facebook-login';
+import axios from "axios";
 
-const parseUrl = require('parseurl')
+const parseUrl = require('parseurl');
 
 
 class Login extends React.Component {
@@ -81,16 +82,23 @@ class Login extends React.Component {
         // &redirect_uri={redirect-uri}
         // &state={state-param}
         const app_id = "189391966126165";
+        // const app_secret = "eeed321129d37dc3a1c82df523f55728";
         // const redirect_uri = encodeURIComponent(chrome.identity.getRedirectURL("TasteMaker"));
-        const redirect_uri = "https://www.facebook.com/connect/login_success.html";
+        // const redirect_uri = "https://www.facebook.com/connect/login_success.html";
+        const redirect_uri = "https://tastemaker.mailchimpsites.com/";
         console.log("redirect_uri is: ", redirect_uri);
-        const redirectRe = new RegExp(redirect_uri + '[#\?](.*)');
-        const state_param = "state_param";
-        const url = `https://www.facebook.com/v9.0/dialog/oauth?client_id=${app_id}&redirect_uri=${redirect_uri}&state=${state_param}`;
+        // const state_param = "state_param";
+        const url = 'https://www.facebook.com/v9.0/dialog/oauth?' +
+        "client_id=" + app_id +
+        "&display=popup" + 
+        // "&response_type=token" +
+        "&access_type=online" +
+        "&scope=public_profile,user_friends,email" + 
+        "&redirect_uri=" + redirect_uri;
+
         console.log("url for FB login is: ", url);
-       
 
-
+        let access_token = null;
         chrome.identity.launchWebAuthFlow(
             {
                 "url": url,
@@ -98,13 +106,49 @@ class Login extends React.Component {
             },
             function (redirectUri) {
                 if (chrome.runtime.lastError) {
-                    callback(new Error(chrome.runtime.lastError));
+                    console.log(chrome.runtime.lastError.message);
                     return;
                 }
-                const response = parseUrl(redirectUri);
-                console.log("redirectUri: ", redirectUri);
-                access_token = response[`#access_token`];
-                callback(null, access_token);
+                console.log("response: ", redirectUri);
+                // Upon success the response is appended to redirectUri, e.g.
+                // https://{app_id}.chromiumapp.org/provider_cb#access_token={value}
+                //     &refresh_token={value}
+                // or:
+                // https://{app_id}.chromiumapp.org/provider_cb#code={value}
+                const response = new URL(redirectUri).searchParams.toString().split("&").reduce((previous, current) => { const [key, value] = current.split("="); previous[key] = value; return previous }, {})
+                console.log("response: ", response);
+                if (response.hasOwnProperty('access_token')) {
+                    access_token = response.access_token;
+                    console.log("Got access_token: ", access_token);
+                }
+                else if (response.hasOwnProperty('code')) {
+                    console.log("Got code: ", response.code);
+                    access_token = exchangeCodeForToken(response.code);
+                }
+
+                function exchangeCodeForToken(code) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET',
+                        // 'https://www.facebook.com/dialog/oauth?'+
+                        'https://graph.facebook.com/v9.0/oauth/access_token?' +
+                        'client_id=' + app_id +
+                        '&redirect_uri=' + redirectUri +
+                        '&client_secret=' + app_secret +
+                        '&code=' + code);
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    xhr.setRequestHeader('Accept', 'application/json');
+                    xhr.onload = function () {
+                        if (this.status === 200) {
+                            var response = JSON.parse('"' + this.responseText + '"');
+                            access_token = response.substring(0, response.indexOf('&'));
+                            console.log("access_token: ", access_token);
+                        }
+                    };
+                    xhr.send();
+                }
+                
+
+                // callback(null, access_token);
             });
 
 
